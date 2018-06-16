3cc61b0064943bf5d740136f1742cd33b2431e1c
Morgan Sipe <morgan.talk@gmail.com>
2018-06-16 12:59:58 -0400

    Added basic getString functionality

--- LowKey.Core/Source/Api/GetArtistInfoRequest.cs
+++ LowKey.Core/Source/Api/GetArtistInfoRequest.cs
@@ -1,13 +1,15 @@
 ﻿namespace LowKey.Core.Source.Api {
   public class GetArtistInfoRequest {
-    public GetArtistInfoRequest(string artistName) {
+    public GetArtistInfoRequest(string artistName, string key) {
       mArtistName = artistName;
+      mKey = key;
     }
 
     public string GetString() {
-      return mArtistName;
+      return string.Format("http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist={0}&api_key={1}&format=json", mArtistName, mKey);
     }
 
     private string mArtistName;
+    private string mKey;
   }
 }
--- LowKey.UnitTests/Source/Api/GetArtistInfoRequestTest.cs
+++ LowKey.UnitTests/Source/Api/GetArtistInfoRequestTest.cs
@@ -6,8 +6,8 @@ namespace LowKey.UnitTests.Source.Api {
   class GetArtistInfoRequestTest {
     [Test]
     public void TestGetArtistInfoRequestPutsCorrectStringTogether() {
-      var request = new GetArtistInfoRequest("test artist");
-      Assert.That(request.GetString(), Is.EqualTo("test artist"));
+      var request = new GetArtistInfoRequest("testArtist", "key2");
+      Assert.That(request.GetString(), Is.EqualTo("http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=testArtist&api_key=key2&format=json"));
       
     }
   }
