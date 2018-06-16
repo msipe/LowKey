d765a05d08aa826afbc3f0083a2b16ed87fe522d
Morgan Sipe <morgan.talk@gmail.com>
2018-06-16 19:50:51 -0400

    Cleaned up usage so there's only 1 call

--- LowKey/Program.cs
+++ LowKey/Program.cs
@@ -15,9 +15,10 @@ namespace LowKey {
       var handler = new RequestHandler(httpClient);
       var runner = new SimilarArtistHandler(handler);
       var request = new GetSimilarArtistsRequest("metallica", "key");
-      Console.WriteLine(runner.InitSimilarArtistsRequest(request).SimilarArtists.Artist[0].Name);
-      Console.WriteLine(runner.InitSimilarArtistsRequest(request).SimilarArtists.Artist[1].Name);
-      Console.WriteLine(runner.InitSimilarArtistsRequest(request).SimilarArtists.Artist[2].Name);
+      var results = runner.InitSimilarArtistsRequest(request);
+      Console.WriteLine(results.SimilarArtists.Artist[0].Name);
+      Console.WriteLine(results.SimilarArtists.Artist[1].Name);
+      Console.WriteLine(results.SimilarArtists.Artist[2].Name);
       //var response = handler.SendRequest(request);
       //var result = handler.ReadRequest(response);
 
